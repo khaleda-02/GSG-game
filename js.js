2d2fd6cc4c1fc6e765de9641c90d046dbd81ba2d
hp <kha>
2022-07-07 00:55:00 +0300

    debug the images

--- js.js
+++ js.js
@@ -7,76 +7,75 @@ let firstImage = document.querySelector(".first-image");
 let secImage = document.querySelector(".sec-image");
 let playerChoice = "";
 let computerChoice = "";
-let options = ["rock", "paper", "scisser"];
+let options = ["Rock", "Paper", "scisser"];
 let images = ["images/rock.png", "images/paper.png", "images/scisser.png"];
 
 //create btns and append it to the page
-options.forEach(function (option) {
-  let element = document.createElement("button");
-  element.innerHTML = option;
-  element.classList.add(option);
-  element.classList.add("option");
-  btnsContainer.appendChild(element);
-  console.log(element.className);
+options.forEach(function(option) {
+    let element = document.createElement("button");
+    element.innerHTML = option;
+    element.classList.add(option);
+    element.classList.add("option");
+    btnsContainer.appendChild(element);
+    console.log(element.className);
 });
-
 // change result spane state
 let btns = document.querySelectorAll(".option");
-btns.forEach(function (btn) {
-  btn.onclick = function () {
-    playerChoice = btn.innerHTML;
-    playerSpan.innerHTML = playerChoice;
-    computerChoice = computerChoiceCreater(options);
-    computerSpan.innerHTML = computerChoice;
-    resultSpan.innerHTML = winner(computerChoice, playerChoice);
-    firstImage.src = "images/" + playerChoice + ".png ";
-    secImage.src = "images/" + computerChoice + ".png ";
-    console.log(firstImage);
-  };
+btns.forEach(function(btn) {
+    btn.onclick = function() {
+        playerChoice = btn.innerHTML;
+        playerSpan.innerHTML = playerChoice;
+        computerChoice = computerChoiceCreater(options);
+        computerSpan.innerHTML = computerChoice;
+        resultSpan.innerHTML = winner(computerChoice, playerChoice);
+        firstImage.src = "images/" + playerChoice + ".png ";
+        secImage.src = "images/" + computerChoice + ".png ";
+        console.log(firstImage);
+    };
 });
 
 function winner(computerChoice, playerChoice) {
-  if (computerChoice != null && playerChoice != null) {
-    // same input
-    if (computerChoice === playerChoice) {
-      return "same choises";
-    }
+    if (computerChoice != null && playerChoice != null) {
+        // same input
+        if (computerChoice === playerChoice) {
+            return "same choises";
+        }
 
-    // Rock
-    if (computerChoice === "Rock") {
-      if (playerChoice === "scisser") {
-        return "computer wins";
-      } else {
-        return "you win";
-      }
+        // Rock
+        if (computerChoice === "Rock") {
+            if (playerChoice === "scisser") {
+                return "computer wins";
+            } else {
+                return "you win";
+            }
+        }
+        // Paper
+        else if (computerChoice === "Paper") {
+            if (playerChoice === "Rock") {
+                return "computer win";
+            } else {
+                return "you win";
+            }
+        }
+        // Scissors
+        else if (computerChoice === "scisser") {
+            if (playerChoice === "Paper") {
+                return "computer win";
+            } else {
+                return "you win";
+            }
+        }
     }
-    // Paper
-    else if (computerChoice === "Paper") {
-      if (playerChoice === "Rock") {
-        return "computer win";
-      } else {
-        return "you win";
-      }
-    }
-    // Scissors
-    else if (computerChoice === "scisser") {
-      if (playerChoice === "Paper") {
-        return "computer win";
-      } else {
-        return "you win";
-      }
-    }
-  }
 }
 
 function computerChoiceCreater(arr) {
-  let selected = arr[Math.floor(Math.random() * arr.length)];
-  return selected;
+    let selected = arr[Math.floor(Math.random() * arr.length)];
+    return selected;
 }
-playAgin.onclick = function () {
-  playerSpan.innerHTML = "Please select any option";
-  computerSpan.innerHTML = "";
-  resultSpan.innerHTML = "";
-  firstImage.src = "images/all.jpg ";
-  secImage.src = "images/all.jpg ";
+playAgin.onclick = function() {
+    playerSpan.innerHTML = "Please select any option";
+    computerSpan.innerHTML = "";
+    resultSpan.innerHTML = "";
+    firstImage.src = "images/all.jpg ";
+    secImage.src = "images/all.jpg ";
 };
